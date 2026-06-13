be89c8b7fabd89fb9cd072a146b0cd906fe3358f
farhana-ayesha <afrworkid2026@gmail.com>
2026-06-13 17:35:50 +0600

    Initial commit

---
+++ script.js
@@ -0,0 +1,33 @@
+/*let buttons = document.querySelectorAll(".btn");
+buttons.forEach(function(button) {
+    button.onclick = function(){
+    button.innerText ="Clicked";
+    };
+});*/
+
+let buttons = document.querySelectorAll(".btn");
+buttons.forEach(function(button){
+button.onclick=function(){
+    button.innerText="Added";
+    button.style.backgroundColor="green"; 
+};
+});
+
+/*alert(Product added to Cart!);
+
+let title=document.querySelector("h2");
+title.innerText = "New Product";
+
+let count = 0
+button.onclick = function(){
+    count++;
+    button.innerText=count;
+}
+
+let cart = 0
+button.onclick = function(){
+    cart++;
+    console.log("Cart:", cart);
+}*/
+
+    
